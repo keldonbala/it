3f1223d4ed1002f8c364d5a357f6cbd8e67ebdac
Keldon Bala <keldon.bala.it@gmail.com>
2019-09-10 21:37:40 -0400

    Initial Commit

---
+++ banner.py
@@ -0,0 +1,16 @@
+
+def banner(title, author):
+    title_length = len(title)
+    by_line = f"By {author}"
+    by_line_length = len(by_line)
+    banner_length = max(title_length, by_line_length) + 4
+    print(f"{'=' * banner_length}")
+    print(f"{title.upper():^{banner_length}}")
+    print(f"{by_line:^{banner_length}}")
+    print(f"{'=' * banner_length}")
+    print("")
+
+banner("BANNER", "Keldon")
+author = input("What is your name?")
+title = input("What is your quest?")
+banner(title, author)
---
+++ bestfriend.py
@@ -0,0 +1,15 @@
+# Best Friend Maker
+print("=========================")
+print("     BEST FRIEND MAKER   ")
+print("=========================")
+print("")
+
+print("Hi. I'm the computer.")
+name = input("What's your name? ")
+color = input(f"Hi, {name}! What's your favorite color? ")
+print(f"No way! My favorite color is also {color}")
+movie = input("What's your favorite movie? ")
+print(f"What!?! My favorite movie is also {movie}!")
+game = input("What is your favorite game? ")
+print(f"Get out of here! My favorite game is {game}, too!")
+print("We like the same things! We should be best friends!")
